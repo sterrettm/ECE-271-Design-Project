40335086ac71ed822eab9018fd134ed9d10cc61b
mdeane84 <mdeane84@yahoo.com>
2019-12-05 13:15:48 -0800

    fixed input select, removed prev from input select

--- Source Files/inputselect.sv
+++ Source Files/inputselect.sv
@@ -1,35 +1,17 @@
 module inputselect (	input logic clk,
 							input logic next,
-							input logic prev,
 							input logic reset,
 							output logic [1:0] sel);
-							
-	typedef enum logic [1:0] {A, B, C} estate;
-	estate currentState, nextState;
 	
-	always_ff@(posedge clk, posedge reset)
-		begin
-			if (reset) 	nextState = A;
-			currentState = nextState;
-		end
-		
-	always_ff@(posedge next, posedge prev)
-		case(currentState)
-			A:	if (next)		nextState = B;
-				else if (prev)	nextState = C;
-				else				nextState = A;
-			
-			B:	if (next)		nextState = C;
-				else if (prev) nextState = A;
-				else				nextState = B;
-			
-			C: if (next)		nextState = A;
-				else if (prev)	nextState = B;
-				else				nextState = C;
-			
-			default:				nextState = A;
-		endcase
-		
-	assign sel = currentState;
+	logic [1:0] state;
+	
+	always_ff@(posedge next)
+		state <= state + 2'b01;
+	
+	sync sync_1 (
+	.clk(clk),
+	.d(state),
+	.q(sel)
+	);
 		
 endmodule
